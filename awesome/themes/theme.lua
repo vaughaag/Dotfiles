39ee50e02d5bcee54cedc14dcbc548af872b2ebf
vaughaag <98517568+vaughaag@users.noreply.github.com>
2022-10-21 16:48:22 +0100

    Updated theme.lua

--- awesome/themes/theme.lua
+++ awesome/themes/theme.lua
@@ -15,7 +15,7 @@ local os = os
 local my_table = awful.util.table or gears.table -- 4.{0,1} compatibility
 
 local theme                                     = {}
-theme.confdir                                   = os.getenv("HOME") .. "/.config/awesome/themes/multicolor"
+theme.confdir                                   = os.getenv("HOME") .. "/.config/awesome/themes"
 theme.wallpaper                                 = theme.confdir .. "/wall.png"
 theme.font                                      = "Hack Nerd Font Mono 10"
 theme.menu_bg_normal                            = "#000000"
