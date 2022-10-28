42976b5a3117f954144b210f57ee6da133e8921d
vaughaag <98517568+vaughaag@users.noreply.github.com>
2022-10-28 02:44:02 +0100

    Update autostart.sh

--- awesome/autostart.sh
+++ awesome/autostart.sh
@@ -7,10 +7,11 @@ function run {
   fi
 }
 
-xrandr --output DisplayPort-0 --mode 1920x1200 --pos 3840x0 --rotate normal --output DisplayPort-1 --mode 1920x1200 --pos 0x0 --rotate normal --output DisplayPort-2 --primary --mode 1920x1200 --pos 1920x0 --rotate normal --output HDMI-A-0 --off --output DVI-D-0 --off
+# cMP Monitor Config
+# xrandr --output DisplayPort-0 --mode 1920x1200 --pos 3840x0 --rotate normal --output DisplayPort-1 --mode 1920x1200 --pos 0x0 --rotate normal --output DisplayPort-2 --primary --mode 1920x1200 --pos 1920x0 --rotate normal --output HDMI-A-0 --off --output DVI-D-0 --off
 
 run nm-applet
-run pamac-tray
+# run pamac-tray widget in theme.lua now checkgin update numbers
 run blueman-applet
 run numlock on
 run volumeicon
